a96709a8afdccaa29cdebfb6fa2e2abb05f4abba
jimeaybar <jimeaybar1999@gmail.com>
2026-04-10 20:53:22 -0300

    Punto 4 completado

--- tp1_1.c
+++ tp1_1.c
@@ -4,5 +4,14 @@ int main(){
 
     printf("Hola mundo\n");
 
+    int numero = 100;
+    int *p_numero = &numero;
+
+    printf("1) Contenido del puntero: %d\n", *p_numero);
+    printf("2) Direccion de memoria almacenada por el puntero: %p\n", (void*)p_numero);
+    printf("3) Direccion de memoria de la variable: %p\n", (void*)&numero);
+    printf("4) Direccion de memoria del puntero: %p\n", (void*)&p_numero);
+    printf("5) Tamanio de memoria de la variable: %zu bytes\n", sizeof(numero));
+
     return 0;
 }
